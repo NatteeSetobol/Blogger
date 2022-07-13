5edd1c577963ece43d0d5124d26aa38f70dc731e
Nattee Setobol <ndsetobol@gmail.com>
2022-07-12 19:42:58 -0700

    testing

--- Backend/src/main/java/com/nomication/Controllers/LoginController.java
+++ Backend/src/main/java/com/nomication/Controllers/LoginController.java
@@ -1,5 +1,23 @@
 package com.nomication.Controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.nomication.Models.Blogger;
+
 public class LoginController {
 
+	@RequestMapping(value="/logon", method = RequestMethod.POST,consumes = {MediaType.APPLICATION_JSON_VALUE})	 
+	public ResponseEntity<Object> Logon(@PathVariable("user") Blogger user,HttpServletRequest httpServletRequest)
+	{
+					
+		return null;
+		
+	}
+	
 }
